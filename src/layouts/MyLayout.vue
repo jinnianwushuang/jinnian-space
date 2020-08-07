217833c3979285a67aa604ecc2560cee628fd448
jinnian <478640949@qq.com>
2020-08-08 07:56:26 +0800

    改变布局

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <!-- <q-header elevated>
       <div class="row">
         <div class="col-12">
           <q-tabs
@@ -18,17 +18,44 @@
             />
           </q-tabs>
         </div>
-        <!-- <div class="col-1 q-pr-lg text-h5 text-right vertical-middle" style="line-height: 48px;">
-锦年空间
-        </div> -->
+
       </div>
-      <!-- <q-toolbar>
-        <q-space />
-        <div class="text-h5">
-          锦年空间
-        </div>
-      </q-toolbar> -->
-    </q-header>
+
+    </q-header> -->
+
+
+      <q-header elevated class="bg-primary text-white">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>锦年成长空间</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+        content-class="bg-grey-3"
+      >
+        <q-scroll-area class="fit">
+          <q-list v-for="(item, index) in menu" :key="index">
+
+            <q-item clickable :active="item.name == current_menu " v-ripple @click="go_to_router(item)">
+              <!-- <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section> -->
+              <q-item-section>
+                {{  `${index + 1}.${item.name}` }}
+              </q-item-section>
+            </q-item>
+ 
+
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+
     <q-page-container class="q-px-lg">
       <router-view />
       <q-page-scroller
@@ -47,7 +74,9 @@ export default {
   name: "MyLayout",
   data() {
     return {
+      drawer: false,
       menu,
+      current_menu:"web",
       tab: "web",
       tabs: [1, 2, 3, 4, 5, 6, 7, 8],
       leftDrawerOpen: false
@@ -58,6 +87,7 @@ export default {
   },
   methods: {
     go_to_router(item) {
+      this.current_menu=item.name
       this.$router.push({
         name: item.name
       });
