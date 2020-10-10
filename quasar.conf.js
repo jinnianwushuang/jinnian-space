06ec0aedafe11a8bcec575da08e5d75feb06a8ba
jinnianwushuang <478640949@qq.com>
2020-10-10 08:19:20 +0800

    新指定输出目录

--- quasar.conf.js
+++ quasar.conf.js
@@ -61,7 +61,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      distDir:"./d",
+      distDir:"./doc",
       scopeHoisting: true,
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       showProgress: true,
