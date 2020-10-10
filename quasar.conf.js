1aa563801962b26fa17c887008a888ffa731d619
jinnianwushuang <478640949@qq.com>
2020-10-10 08:23:57 +0800

    111

--- quasar.conf.js
+++ quasar.conf.js
@@ -61,7 +61,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      distDir:"./doc",
+      distDir:"./docs",
       scopeHoisting: true,
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       showProgress: true,
