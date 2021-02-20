e7b62ae854baf7e252d4a9b8c27baec4be999de7
jinnianwushuang <478640949@qq.com>
2021-02-20 08:44:03 +0800

    20210220

--- src/pages/java/module/m2.vue
+++ src/pages/java/module/m2.vue
@@ -42,7 +42,29 @@
        </pre>
        <div class="text-h6"> 数据类型 </div>
        <pre>
-           java 有字符串 和 字符 区别  字符串 双引号 任意个字符    字符 单引号 且只能1个字符
+           java 有字符串 和 字符 区别  字符串 双引号 任意个字符    字符 单引号 且只能1个字符 
+          不同数据类型  占用内存空间
+          byte 1 short 2 int 4 long 8     float 4 double 8  char 2 boolean 1 
+          引用数据类型： class interface  array
+          基本数据类型  整数 浮点数 字符  布尔 
+          int a = 1;
+          float b = 1.36F; float 类型 不加 F 标识， 则默认 double 类型转换 会报错 丢失精度
+          long c = 556188L; long 类型  后面加 L 标识
+        数据范围关系 从小到大
+        byte, (short=char) , int ,long ,float ,double 
+        隐式转换 小的直接变大的
+        运算过程中，小的会先提升为大的，当类型统一之后，再进行运算 。 同类型 运算
+        byte short char 三种数据类型在运算的时候，不管是否有更高的数据类型，都会提升为 int 然后再运算
+        也就是 最低隐士转换为 int 类型 ， 结果 最低是 int 类型
+        强制转换  大的转小的 强转格式 ： int a=10; byte b= (byte)a;   强转可能丢失精度
+        byte d =3+4 ; java 中存在常量优化机制：
+         根据常量计算的结果 判定是否在类型范围内，在就编译通过，不在就报错
+            java 变量作用域： 只在 变量所在的大括号内有用 
+         java 整数相除 结果是整数  10/3 = 3    10/3.0 = 3.333333...
+         	 char b = 'a';	 int c= b +20;  结果  c = 117 ; 字节 a 对应的 ASCII 码表 97 
+              1+99+“年” = "100年"
+	
+
        </pre>
     </div>
 </template>
