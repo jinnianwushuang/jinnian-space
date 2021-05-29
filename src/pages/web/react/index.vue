15524778378bc4c22e0c3ddab8e8e718e2b117f6
jinnianwushuang <478640949@qq.com>
2021-05-29 23:11:24 +0800

    d20210529
    d20210529

--- src/pages/web/react/index.vue
+++ src/pages/web/react/index.vue
@@ -1,11 +1,7 @@
-<!--
- * @Date           : 2021-04-12 16:09:51
- * @FilePath       : /jinnian-space/src/pages/web/react/index.vue
- * @Description    : 
--->
+
 <!--
  * @Date           : 2020-08-31 16:40:04
- * @FilePath       : /jinnian-space/src/pages/java/index.vue
+ * @FilePath       : /jinnian-space/src/pages/web/react/index.vue
  * @Description    : 
 -->
 <template>
@@ -29,6 +25,7 @@ import m1 from "./module/m1.vue";
 import m2 from "./module/m2.vue";
 import m3 from "./module/m3.vue";
 import m4 from "./module/m4.vue";
+import m5 from "./module/m5.vue";
 
 
 export default {
@@ -37,6 +34,7 @@ export default {
     m2,
     m3,
     m4,
+    m5
 
 
 
@@ -49,6 +47,7 @@ export default {
         { label: "基础", value: "m2" },
         { label: "基础", value: "m3" },
         { label: "基础demo", value: "m4" },
+        { label: "核心精讲", value: "m5" },
 
 
       ],
--- src/pages/web/react/module/m5.vue
+++ src/pages/web/react/module/m5.vue
@@ -1,15 +1,95 @@
+<!--
+ * @Date           : 2020-08-31 16:40:04
+ * @FilePath       : /jinnian-space/src/pages/web/react/module/m5.vue
+ * @Description    : 
+-->
+
+<style lang="scss" scoped></style>
 <template>
-    <div>
-        1
+  <div>
+    <div class="row q-gutter-md q-my-md">
+      <q-btn
+        color="primary"
+        v-for="(item, index) in book_arr"
+        :key="`book_arr_${index}`"
+        :label="item"
+        @click="change_book(item)"
+      />
     </div>
+
+    <div class="container q-pa-lg">
+      <!-- type="pdfjs"     type="html5" -->
+      <q-pdfviewer
+        v-model="show"
+        :src="src"
+        type="html5"
+        content-class="fit container"
+        inner-content-class="fit container"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import  util from "src/util/index.js"
+export default {
+  name: "Container",
+
+  data() {
+    return {
+      show: true,
+      src:   util.compute_book_src("react/react核心精讲/02_03_04_JSX核心语法.pdf") ,
+      book_arr: [
+"01_.pdf",
+"02_03_04_JSX核心语法.pdf",
+"05_React脚手架.pdf",
+"06_07_08__09_10_React组件化开发.pdf",
+"11_React的CSS.pdf",
+"12_AntDesignUI库.pdf",
+"13_axios的使用.pdf",
+"14_React过渡动画.pdf",
+"15_16_17_Redux的使用.pdf",
+"18_19_React-router的使用.pdf",
+"20_21_22_React-Hooks.pdf",
+"23_24_网易云项目实战.pdf",
+"30_React-SSR.pdf",
+"项目自动化部署.pdf",
+      ]
+    };
+  },
+  methods: {
+    change_book(item) {
+      this.src =   util.compute_book_src("react/react核心精讲/" + item) ;
+    },
+    fetchPDF (payload) {
+  this.$axios.post('/my/url/to/pdf', payload, { responseType: 'blob' }).then(res => {
+    // create the blob
+    const blob = new Blob([res.data], { type: res.data.type })
+    // set reactive variable
+    this.src = window.URL.createObjectURL(blob)
+  }).catch(err => {
+    this.$q.notify({
+      message: 'Error downloading PDF',
+      type: 'negative',
+      textColor: 'white',
+      color: 'negative',
+      icon: 'error',
+      closeBtn: 'close',
+      position: 'top'
+    })
+  })
+}
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.container {
+  max-width: 80vw;
+  max-height: 100vh;
+  min-width: 70vw;
+  min-height: 70vh;
+  width: 100%;
+  height: 100%;
+}
 </style>
